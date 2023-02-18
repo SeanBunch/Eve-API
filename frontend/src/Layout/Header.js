9377e1c501d2a1d0050f775fdd4d24521b5217f8
Sean Bunch <100799798+SeanBunch@users.noreply.github.com>
2023-02-18 18:02:53 -0500

    Unfimilar React installed. need to fix

---
+++ frontend/src/Layout/Header.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+function Header() {
+
+    return (
+        <div className="App">
+        <header className="App-header">
+          <p>
+            Eve Market API
+          </p>
+          
+        </header>
+      </div>
+    )
+}
+
+export default Header;
