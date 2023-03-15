71a1f7c15265a4c8076cfbd99d9117ce5e84066c
Sean Bunch <100799798+SeanBunch@users.noreply.github.com>
2023-03-14 20:36:03 -0400

    component updates

--- frontend/src/Layout/Layout.js
+++ frontend/src/Layout/Layout.js
@@ -1,26 +1,21 @@
 import React from "react";
-import { Routes, Route } from "react-router-dom";
+// import { Routes, Route } from "react-router-dom";
 import MarketWindow from "../MarketWindow/MarketWindow";
-import Header from "./Header";
 import Menu from "./Menu";
 
 function Layout() {
-
   return (
     <div className="container-fluid">
       <div className="row h-100">
         <div className="col-md-2 side-bar">
-        <Header />
+          <Menu />
         </div>
         <div className="col">
-          <Menu />
           <MarketWindow />
-
-
         </div>
       </div>
     </div>
-  )
+  );
 }
 
 export default Layout;
